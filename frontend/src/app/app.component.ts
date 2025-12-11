a42d25eebdb190219957273c2e7cc9fb1454a55a
Cathal <cathal.flanagan12@gmail.com>
2025-12-11 00:45:36 +0000

    asb

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { AuthService } from './services/auth.service';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,   
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  readonly user$ = this.auth.user$;
-
-  constructor(private auth: AuthService) {}
-
-  logout(): void {
-    this.auth.logout();
-  }
+  title = 'goalline-frontend';
 }
